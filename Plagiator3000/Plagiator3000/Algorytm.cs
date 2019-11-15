5a79a5a233e6e9ed2274406d8c4d28e058350cdc
Filip <fifigaw98@gmail.com>
2019-11-15 19:26:41 +0100

    work for more copies

--- Plagiator3000/Plagiator3000/Algorytm.cs
+++ Plagiator3000/Plagiator3000/Algorytm.cs
@@ -8,68 +8,60 @@ namespace Plagiator3000
 {
     static class Algorytm
     {
-        public static double CosineDistance(int[,] array2D)
+        public static double[] CosineDistance(int[,] array2D)
         {
-            double licznik = 0;
-            double mianownik = 1;
-            double[] licznikSumy = new double[array2D.GetUpperBound(1) + 1];
-            double[] mianSumy = new double[array2D.GetUpperBound(0) + 1];
-            for (int i = 0; i < licznikSumy.Length; i++)
-            {
-                licznikSumy[i] = 1;
-            }
-            for (int i = 0; i < mianSumy.Length; i++)
-            {
-                mianSumy[i] = 0;
-            }
+            double licznik;
+            double mianownik;
+            double mianOrig = 0; //pierwiastek(ai ^2)
+            double mianCopy; //pierwiastek(bi ^2)
+            // Length cosDistance jest rowna lb kopii
+            double[] cosDistance = new double[array2D.GetUpperBound(0)];
+
+            //petla ktora wykona sie jeden raz
             for (int y = 0; y <= array2D.GetUpperBound(1); y++)
             {
-                for (int x = 0; x <= array2D.GetUpperBound(0); x++)
-                {
-                    licznikSumy[y] *= array2D[x, y];
-                }
+                mianOrig += Math.Pow(array2D[0, y], 2);
             }
-            for (int x = 0; x <= array2D.GetUpperBound(0); x++)
+            mianOrig = Math.Sqrt(mianOrig);
+            for(int x = 1; x <= array2D.GetUpperBound(0); x++)
             {
+                mianCopy = 0;
+                licznik = 0;
                 for (int y = 0; y <= array2D.GetUpperBound(1); y++)
                 {
-                    mianSumy[x] += Math.Pow(array2D[x, y], 2);
+                    licznik += array2D[0, y] * array2D[x, y];
                 }
+                for (int y = 0; y <= array2D.GetUpperBound(1); y++)
+                {
+                    mianCopy += Math.Pow(array2D[x, y], 2);
+                }
+                mianCopy = Math.Sqrt(mianCopy);
+                mianownik = mianOrig * mianCopy;
+                double cosSimilarity = licznik / mianownik;
+                cosDistance[x - 1] = 1 - cosSimilarity;
             }
-            for (int i = 0; i < licznikSumy.Length; i++)
-            {
-                licznik += licznikSumy[i];
-            }
-            for (int i = 0; i < mianSumy.Length; i++)
-            {
-                mianownik *= Math.Sqrt(mianSumy[i]);
-            }
-            double cosSimilarity = licznik / mianownik;
-            double cosDistance = 1 - cosSimilarity;
             return cosDistance;
         }
-        public static double EuclideanDistance(int[,] array2D)
+        public static double[] EuclideanDistance(int[,] array2D)
         {
+            double[] EuclDist = new double[array2D.GetUpperBound(0) + 1];
             //roznica czestosci wystepowan posczegolnych liń dla oryg i 1 kopii
             int[] roznica = new int[array2D.GetUpperBound(1) + 1];
-            int sumaRoznic = 0;
-            double result = 0.0;
-            //for (int i = 0; i < mianSumy.Length; i++)
-            //{
-            //    mianSumy[i] = 0;
-            //}
+            int sumaRoznic;
+            double result;
             //od 1 bo dokument 0 to oryginal do ktorego wszystko bedzie porownywane
             for (int x = 1; x <= array2D.GetUpperBound(0); x++)
             {
+                sumaRoznic = 0;
                 for (int y = 0; y <= array2D.GetUpperBound(1); y++)
                 {
                     roznica[y] = array2D[0, y] - array2D[x, y];
                     roznica[y] *= roznica[y];
                     sumaRoznic += roznica[y];
                 }
+                EuclDist[x-1] = Math.Sqrt(sumaRoznic);
             }
-            result = Math.Sqrt(sumaRoznic);
-            return result;
+            return EuclDist;
         }
     }
 }
--- Plagiator3000/Plagiator3000/Form1.cs
+++ Plagiator3000/Plagiator3000/Form1.cs
@@ -18,8 +18,8 @@ namespace Plagiator3000
             InitializeComponent();
             Dictionary<String, int> countsOriginal = CountLines(@"gawel_testy/oryg.txt");
             Dictionary<String, int> countsCopy1 = CountLines(@"gawel_testy/copy1.txt");
-            //Dictionary<String, int> countsCopy2 = CountLines(@"gawel_testy/copy2.txt");
-            List<Dictionary<String, int>> copies = new List<Dictionary<String, int>>() { countsCopy1};
+            Dictionary<String, int> countsCopy2 = CountLines(@"gawel_testy/copy2.txt");
+            List<Dictionary<String, int>> copies = new List<Dictionary<String, int>>() { countsCopy1, countsCopy2 };
             int[,] array2D = ToArray(countsOriginal, copies);
             //jedynka juz zajeta przez oryginal
             for (int y = 0; y <= array2D.GetUpperBound(1); y++)
@@ -30,9 +30,9 @@ namespace Plagiator3000
                 }
                 textBox1.Text += "\r\n";
             }
-            textBox1.Text += Algorytm.CosineDistance(array2D);
+            textBox1.Text += Algorytm.CosineDistance(array2D)[1];
             textBox1.Text += "   ";
-            textBox1.Text += Algorytm.EuclideanDistance(array2D);
+            textBox1.Text += Algorytm.EuclideanDistance(array2D)[1];
         }
         private Dictionary<String, int> CountLines(String path)
         {
