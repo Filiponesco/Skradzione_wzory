5d7220270f4ae3e0f7fbf8861f38bcce4003c304
Matikones <matiadamek97@gmail.com>
2019-11-18 14:10:30 +0100

    Load_All

--- Plagiator3000/Plagiator3000/Model.cs
+++ Plagiator3000/Plagiator3000/Model.cs
@@ -14,12 +14,12 @@ namespace Plagiator3000
         string path_dir = "d:\\";
         public string Load_Orig_Latex() //wczytywanie pliku z oryginalnym latexem
         {
-            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.InitialDirectory = "c:\\";
                 openFileDialog.RestoreDirectory = true;
 
-                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     path = openFileDialog.FileName;
                 }
@@ -31,12 +31,12 @@ namespace Plagiator3000
         {
             using (FolderBrowserDialog win = new FolderBrowserDialog())
             {
-                if(win.ShowDialog() == DialogResult.OK)
+                if (win.ShowDialog() == DialogResult.OK)
                 {
                     path_dir = win.SelectedPath;
                 }
             }
-            Load_Plagiat_Files(); //SPR ---------------------------------------------------------
+            // Load_Plagiat_Files(); //SPR ---------------------------------------------------------
             return path_dir;
         }
 
@@ -64,26 +64,20 @@ namespace Plagiator3000
 
             text = File.ReadAllText(File_Latex).Replace(" ", "");
 
-            string[] text_split = text.Split(new char[] {});
-           
+            string[] text_split = text.Split(new char[] { });
+
+
             string[] new_text = new string[text_split.Length];
 
             int j = 0;
             for (int i = 0; i < text_split.Length; i++)
             {
-                if(text_split[i] != "")
+                if (text_split[i] != "")
                 {
                     new_text[j] = text_split[i];
                     j++;
-                } 
+                }
             }
-
-            //Console.WriteLine("\nNowy text:");
-
-            //for (int i = 0; i < j; i++)
-            //{
-            //    Console.WriteLine(i + " el: " + new_text[i]);
-            //}
 
             string[] mat = new string[new_text.Length];
 
@@ -91,33 +85,126 @@ namespace Plagiator3000
 
             for (int i = 0; i < new_text.Length; i++)
             {
-                if(new_text[i] == @"\begin{math}")
+                try
                 {
-                    mat[l] = new_text[i + 1];
-                    l++;
+                    if (new_text[i] == @"\begin{math}")
+                    {
+                        while (new_text[i + 1] != @"\end{math}")
+                        {
+                            mat[l] = new_text[i + 1];
+                            l++;
+                            i++;
+                        }
+                        i++;
+                    }
+                    else if (new_text[i] == @"\begin{displaymath}")
+                    {
+                        while (new_text[i + 1] != @"\end{displaymath}")
+                        {
+                            mat[l] = new_text[i + 1];
+                            l++;
+                            i++;
+                        }
+                        i++;
+                    }
+                    else if (new_text[i] == @"\begin{equation}")
+                    {
+                        while (new_text[i + 1] != @"\end{equation}")
+                        {
+                            mat[l] = new_text[i + 1];
+                            l++;
+                            i++;
+                        }
+                        i++;
+                    }
                 }
-                else if(new_text[i] == @"\begin{displaymath}")
-                {
-                    mat[l] = new_text[i + 1];
-                    l++;
-                }
-                else if(new_text[i] == @"\begin{equation}")
-                {
-                    mat[l] = new_text[i + 1];
-                    l++;
-                }
+                catch { MessageBox.Show("Dokument jest niepoprawny!\nNie znaleziono zamkniecia wyrazenia matematycznego lub niepoprawnie go uzyto!\nReszta operacji w programie moze zawierać błędy!"); }
             }
 
+            string[] znaki = new string[text.Length];
+            string traj = "";
+            j = 0;
+
+            try
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if ((text[i].ToString() == @"$".ToString()) && (text[i + 1].ToString() == @"$".ToString()))
+                    {
+                        i += 2;
+                        while ((text[i].ToString() != @"$".ToString()) && (text[i + 1].ToString() != @"$".ToString()))
+                        {
+                            znaki[j] = text[i].ToString();
+                            j++;
+                            i++;
+                        }
+                        znaki[j] = text[i].ToString();
+                        i++;
+                        j = 0;
+                        traj = String.Join("", znaki);
+                        Array.Clear(znaki, 0, znaki.Length);
+                        mat[l] = traj;
+                        l++;
+                    }
+                    else if ((text[i].ToString() == @"$".ToString()) && (text[i + 1].ToString() != @"$".ToString()) && (text[i - 1].ToString() != @"$".ToString()))
+                    {
+                        while (text[i + 1].ToString() != @"$".ToString())
+                        {
+                            znaki[j] = text[i + 1].ToString();
+                            j++;
+                            i++;
+                        }
+                        i++;
+                        j = 0;
+                        traj = String.Join("", znaki);
+                        Array.Clear(znaki, 0, znaki.Length);
+                        mat[l] = traj;
+                        l++;
+                    }
+                    else if ((Equals(text[i].ToString(), @"\".ToString())) && (Equals(text[i + 1].ToString(), @"(".ToString())))
+                    {
+                        i += 2;
+                        while ((text[i].ToString() != @"\".ToString()) && (text[i + 1].ToString() != @")".ToString()))
+                        {
+                            znaki[j] = text[i].ToString();
+                            j++;
+                            i++;
+                        }
+                        i++;
+                        j = 0;
+                        traj = String.Join("", znaki);
+                        Array.Clear(znaki, 0, znaki.Length);
+                        mat[l] = traj;
+                        l++;
+                    }
+                    else if ((Equals(text[i].ToString(), @"\".ToString())) && (Equals(text[i + 1].ToString(), @"[".ToString())))
+                    {
+                        i += 2;
+                        while ((text[i].ToString() != @"\".ToString()) && (text[i + 1].ToString() != @"]".ToString()))
+                        {
+                            znaki[j] = text[i].ToString();
+                            j++;
+                            i++;
+                        }
+                        i++;
+                        j = 0;
+                        traj = String.Join("", znaki);
+                        Array.Clear(znaki, 0, znaki.Length);
+                        mat[l] = traj;
+                        l++;
+                    }
+                }
+            }
+            catch { MessageBox.Show("Dokument jest niepoprawny!\nNie znaleziono zamkniecia wyrazenia matematycznego lub niepoprawnie go uzyto!\nReszta operacji w programie moze zawierać błędy!"); }
 
             Console.WriteLine("Wyrazenia matematyczne: ");
             for (int i = 0; i < l; i++)
             {
                 Console.WriteLine(i + " el: " + mat[i]);
             }
-
-
             return File_Latex;
         }
+
 
         private void Same_Or_Not() //Porownanie plikow
         {
